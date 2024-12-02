7201ba763e970106b8f41fed1b7413798ef0aebc
DevMac <devmac@DevMacs-MBP.lan>
2024-12-01 23:22:22 -0600

    disable env calls

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,37 +1,37 @@
-import * as Sentry from '@sentry/nextjs';
+// import * as Sentry from '@sentry/nextjs';
 
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    // Node.js Sentry configuration
-    Sentry.init({
-      // Sentry DSN
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+// export async function register() {
+//   if (process.env.NEXT_RUNTIME === 'nodejs') {
+//     // Node.js Sentry configuration
+//     Sentry.init({
+//       // Sentry DSN
+//       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-      // Enable Spotlight in development
-      spotlight: process.env.NODE_ENV === 'development',
+//       // Enable Spotlight in development
+//       spotlight: process.env.NODE_ENV === 'development',
 
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
+//       // Adjust this value in production, or use tracesSampler for greater control
+//       tracesSampleRate: 1,
 
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    });
-  }
+//       // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//       debug: false,
+//     });
+//   }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    // Edge Sentry configuration
-    Sentry.init({
-      // Sentry DSN
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+//   if (process.env.NEXT_RUNTIME === 'edge') {
+//     // Edge Sentry configuration
+//     Sentry.init({
+//       // Sentry DSN
+//       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-      // Enable Spotlight in development
-      spotlight: process.env.NODE_ENV === 'development',
+//       // Enable Spotlight in development
+//       spotlight: process.env.NODE_ENV === 'development',
 
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
+//       // Adjust this value in production, or use tracesSampler for greater control
+//       tracesSampleRate: 1,
 
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    });
-  }
-}
+//       // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//       debug: false,
+//     });
+//   }
+// }
